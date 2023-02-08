b56723bc179e6b379028b28090e6fcdda6b30d4f
IAROSLAV OMELIANENKO <om-y@yandex.ru>
2023-02-08 20:29:58 +0300

    add condition if-else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "bob")  //ToLower приводит к прописным буквам
+{
+    Console.WriteLine("Wow! It's Bob!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
